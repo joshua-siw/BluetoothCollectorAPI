f7eb7f8c2f52c9809755ee322b94277627ad7b96
joshuaSiwek <joshuasiwek@jaemacom.de>
2024-03-09 20:00:37 +0100

    getpageduser UserController.cs

---
+++ BluetoothCollectorAPI/BluetoothCollectorAPI.WebApi/Controllers/v1/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using BluetoothCollectorAPI.Application.DTOs.Account.Requests;
+using BluetoothCollectorAPI.Application.DTOs.Account.Responses;
+using BluetoothCollectorAPI.Application.Interfaces.UserInterfaces;
+using BluetoothCollectorAPI.Application.Wrappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BluetoothCollectorAPI.WebApi.Controllers.v1;
+
+[ApiVersion("1")]
+public class UserController(IGetUserServices getUserServices): BaseApiController
+{
+    [HttpPost]
+    public async Task<PagedResponse<UserDto>> GetPagedListUser(GetAllUsersRequest request)
+        => await getUserServices.GetPagedUsers(request);
+}
